11c1da689599c137989d53ea200b0380063cc623
iwasemo <iwasemo@bc.edu>
2025-01-13 10:18:27 -0500

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Momoka Iwase on 2025/01/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Momoka Iwase on 2025/01/13.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
