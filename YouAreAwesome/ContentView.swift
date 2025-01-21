f9675298d794e1718826962f99f4897405001d8b
iwasemo <iwasemo@bc.edu>
2025-01-21 12:14:30 -0500

    Ch. 1.7 Adding Your Own Images to Your SwiftUI App

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -13,34 +13,25 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Spacer()
-            Image(systemName: imageName)
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
-            
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
             Spacer()
             Button("Press Me!") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
-                let imageName1 = "hand.thumbsup"
-                let imageName2 = "sun.max.fill"
                 
-//                if message == message1 {
-//                    message = message2imageName = imageName2
-//                } else {
-//                    message = message1
-//                    imageName = imageName2
-//                }
-                
-                message = ( message == message1 ? message2 : message1)
-                imageName = (imageName == imageName1 ? imageName2 : imageName1)
+                message = (message == message1 ? message2 : message1)
+                imageName = (imageName == "image0" ? "image1": "image0")
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.orange)
             
         }
         .padding()
