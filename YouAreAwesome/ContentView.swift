dfa422e97df8af43e4fe8a587db5e482c53d6828
iwasemo <iwasemo@bc.edu>
2025-01-27 12:05:36 -0500

    Ch. 1.12 while and repeat while loops in Swift

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messageNumber = 0
+    @State private var lastMessageNumber = -1 //lastMessageNumber will never be -1
+    @State private var lastImageNumber = -1
+    
     var body: some View {
         VStack {
             Text(message)
@@ -36,10 +37,25 @@ struct ContentView: View {
                                 "You are Fantastic",
                                 "Fabulous? That's You!"
                 ]
+                //generate random messageNumber to use as index
+                //if messageNumber == lastMessageNumber {
+                //keep generating messageNumber until messageNumber != lastMessageNumber
+                //set messageString to messages[messageNumber]
+                //update lastMessageNumber with messageNumber
                 
-                message = messages[Int.random(in: 0...messages.count-1 )]
+                var messageNumber: Int
+                repeat {
+                    messageNumber = Int.random(in: 0...messages.count-1 )
+                } while messageNumber == lastMessageNumber
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
                 
-                imageName = "image\(Int.random(in: 0...9))"
+                var imageNumber = Int.random(in: 0...9)
+                while imageNumber == lastImageNumber {
+                    imageNumber = Int.random(in: 0...9)
+                }
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
                  
             }
             .buttonStyle(.borderedProminent)
