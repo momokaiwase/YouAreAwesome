7c8de2072a68f10a6d18477d2c774c1671b7ac35
iwasemo <iwasemo@bc.edu>
2025-01-29 16:23:48 -0500

    Ch. 1.16 A First Refactoring with Functions in SwiftUI

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -44,37 +44,17 @@ struct ContentView: View {
                                 "You are Fantastic",
                                 "Fabulous? That's You!"
                 ]
-
-                var messageNumber: Int
-                repeat {
-                    messageNumber = Int.random(in: 0...messages.count-1 )
-                } while messageNumber == lastMessageNumber
-                message = messages[messageNumber]
-                lastMessageNumber = messageNumber
                 
-                var imageNumber: Int
-                repeat {
-                    imageNumber = Int.random(in: 0...(numberOfImages-1))
-                } while imageNumber == lastImageNumber
-                imageName = "image\(imageNumber)"
-                lastImageNumber = imageNumber
+                lastMessageNumber = nonRepeatingRandom(lastNumber: lastMessageNumber, upperBound: messages.count-1)
+                message = messages[lastMessageNumber]
                 
-                var soundNumber: Int
-                repeat {
-                    soundNumber = Int.random(in: 0...(numberOfSounds-1))
-                } while soundNumber == lastSoundNumber
-                lastSoundNumber = soundNumber
-                let soundName = "sound\(soundNumber)"
+                lastImageNumber = nonRepeatingRandom(lastNumber: lastImageNumber, upperBound: numberOfImages-1)
+                imageName = "image\(lastImageNumber)"
                 
-                guard let soundFile = NSDataAsset(name: soundName)else {
-                    print("😡 Could not read file named \(soundName)") //ctrl cmd space to insert emoji
-                    return }
-                do {
-                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
-                    audioPlayer.play()
-                } catch {
-                    print("😡 Error: \(error.localizedDescription) creating audioPlayer")
-                }
+                
+                lastSoundNumber = nonRepeatingRandom(lastNumber: lastSoundNumber, upperBound: numberOfSounds-1)
+                imageName = "image\(lastImageNumber)"
+                playSound(soundName: "sound\(lastSoundNumber)")
                 
             }
             .buttonStyle(.borderedProminent)
@@ -82,6 +62,25 @@ struct ContentView: View {
             
         }
         .padding()
+    }
+    
+    func nonRepeatingRandom (lastNumber: Int, upperBound: Int) -> Int{
+        var newNumber: Int
+        repeat {
+            newNumber = Int.random(in: 0...(upperBound))
+        } while newNumber == lastNumber
+        return newNumber
+    }
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName)else {
+            print("😡 Could not read file named \(soundName)") //ctrl cmd space to insert emoji
+            return }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 Error: \(error.localizedDescription) creating audioPlayer")
+        }
     }
 }
 
